e998ebfdf224cf41026af5cf20a5d041ca31477c
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2022-02-07 19:02:11 -0500

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,10 @@
+def balanceinquiry(self):
+    print("Your balance is $100")
+
+def cashwithdrawal(self, amount):
+    new_amount = 100-amount
+    print("you withdrawed:" + str(amount) + "Your remaining balance is: " + str(new_amount))
+
+
+def(main):
+        name = input("insert your card number")
